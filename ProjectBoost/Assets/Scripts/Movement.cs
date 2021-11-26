bcb554b55e6ec8dc08b8744f8cbf020634b2b45a
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2021-11-26 18:58:29 +0100

    ProjectBoost

--- ProjectBoost/Assets/Scripts/Movement.cs
+++ ProjectBoost/Assets/Scripts/Movement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     Rigidbody rocketbody;
+    [SerializeField] float mainThrust = 1000f;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,7 +24,7 @@ public class Movement : MonoBehaviour
         
         if (Input.GetKey(KeyCode.Space)) 
         {
-            rocketbody.AddRelativeForce(Vector3.up); //0,1,0 same thing == vector.up
+            rocketbody.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime); //0,1,0 same thing == vector.up
         }
         
     }

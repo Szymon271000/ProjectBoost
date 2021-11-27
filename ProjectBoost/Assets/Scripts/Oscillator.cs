068f1b819034812bf85b85009b239c793b78a5f9
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2021-11-27 22:21:23 +0100

    ProjectBoost

--- ProjectBoost/Assets/Scripts/Oscillator.cs
+++ ProjectBoost/Assets/Scripts/Oscillator.cs
@@ -18,6 +18,11 @@ public class Oscillator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (period <= Mathf.Epsilon) //il piu piccolo float che esiste!!!
+        {
+            return;
+        }
+        
         float cycles = Time.time / period; // continually growing over time
 
         const float tau = Mathf.PI * 2; // constant value of 6,283
@@ -27,5 +32,6 @@ public class Oscillator : MonoBehaviour
 
         Vector3 offSet = movementVector * movementFactor;
         transform.position = startingPosition + offSet;
+           
     }
 }
